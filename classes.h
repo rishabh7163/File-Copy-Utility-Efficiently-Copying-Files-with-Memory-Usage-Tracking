3653b89e4c4d4ef69b446809e58775f8e67ac2ac
rishabh7163 <40897005+rishabh7163@users.noreply.github.com>
2023-05-11 19:51:51 -0700

    Add files via upload

--- classes.h
+++ classes.h
@@ -28,27 +28,30 @@ public:
 
 class StorageBufferManager {
 private:
-    const int BLOCK_SIZE = 4096;
+    const int PAGE_SIZE = 1; // Each line is a page
     int numRecords;
     std::string fileName;
     std::ofstream output_file;
-    
+    int recordsWrittenToPage;
+
     void writeRecordToFile(const Record& record) {
         output_file << record.id << "," << record.name << "," << record.bio << "," << record.manager_id << "\n";
+        recordsWrittenToPage++;
     }
 
 public:
 
     StorageBufferManager(const std::string& newFileName) {
-    fileName = newFileName + ".csv";
-    numRecords = 0;
-    output_file.open(fileName, std::ios::out);
-    std::ifstream input_file("EmployeeRelation.csv");
-    std::string header;
-    getline(input_file, header); // read the header line from the input file
-    output_file << header << "\n"; // write the header line to the output file
-    input_file.close();
-}
+        fileName = newFileName + ".csv";
+        numRecords = 0;
+        recordsWrittenToPage = 0;
+        output_file.open(fileName, std::ios::out);
+        std::ifstream input_file("EmployeeRelation.csv");
+        std::string header;
+        getline(input_file, header); // read the header line from the input file
+        output_file << header << "\n"; // write the header line to the output file
+        input_file.close();
+    }
     
     
     ~StorageBufferManager() {
@@ -75,14 +78,15 @@ public:
             }
             
             Record record(fields);
+            
             writeRecordToFile(record);
             
             numRecords++;
             
-            
-            if (output_file.tellp() > 12000) {
+            if (recordsWrittenToPage >= PAGE_SIZE) {
                 output_file.close();
                 output_file.open(fileName, std::ios::app);
+                recordsWrittenToPage = 0;
             }
         }
         
@@ -90,30 +94,53 @@ public:
     }
     
     Record findRecordById(int id) const {
-        std::ifstream input_file(fileName);
-        std::string line;
-        getline(input_file, line); // read and ignore the header line
-        
-        while (getline(input_file, line)) {
-            std::vector<std::string> fields;
-            std::stringstream ss(line);
-            std::string field;
-            
-            while (getline(ss, field, ',')) {
-                fields.push_back(field);
-            }
-            
-            Record record(fields);
-            
-            if (record.id == id) {
-                input_file.close();
-                return record;
-            }
+    std::ifstream input_file(fileName);
+    std::string line;
+    getline(input_file, line); // read and ignore the header line
+
+    int numLines = 1;
+    while (getline(input_file, line)) {
+        std::vector<std::string> fields;
+        std::stringstream ss(line);
+        std::string field;
+
+        while (getline(ss, field, ',')) {
+            fields.push_back(field);
         }
-        
-        input_file.close();
-        throw std::invalid_argument("Record not found!");
+
+        Record record(fields);
+
+        if (record.id == id) {
+            input_file.close();
+            return record;
+        }
+
+        numLines++;
+
+        // Get the current position in the file
+        std::streampos currentPos = input_file.tellg();
+
+        // Move to the beginning of the file to count the number of lines read so far
+        input_file.seekg(0, std::ios::beg);
+        for (int i = 0; i < numLines; i++) {
+            getline(input_file, line);
+        }
+
+        // Get the current position in the file again
+        std::streampos newPos = input_file.tellg();
+
+        // Calculate the size of the read so far
+        int sizeRead = newPos - currentPos;
+
+        if (sizeRead >= PAGE_SIZE) {
+            input_file.close();
+            throw std::invalid_argument("Record not found!");
+        }
     }
+
+    input_file.close();
+    throw std::invalid_argument("Record not found!");
+}
+
+
 };
-
-
